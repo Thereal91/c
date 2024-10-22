539867b14a5538c859d52f4e77bfa8638236da37
Thereal91 <theboysreal132@gmail.com>
2024-10-22 20:00:05 +0530

    Update arith.c

--- arith.c
+++ arith.c
@@ -19,34 +19,3 @@ avg = (float)(a+b)/2;
 printf("\n\t Average = %f", avg);
 return 0;
 }
-EX:2(a) SIMPLE IF
-/*SIMPLE IF*/
-#include<stdio.h>
-int main()
-{
-int num;
-printf("\n Enter a number: ");
-scanf("%d",&num);
-if(num<0)
-{
-printf("\n Given number is Negative: %d",num);
-}
-printf("\n Try another number\n");
-return 0;
-}
-EX:2(b) IF ELSE
-/*IF ELSE PROGRAM*/
-#include<stdio.h>
-int main()
-{
-int n;
-printf("\n ENTER A NUMBER: ");
-scanf("%d", &n);
-if(n%2==0)
-{
-printf("\n THE GIVEN NUMBER IS EVEN\n");
-}
-else
-printf("\n THE GIVEN NUMBER IS ODD\n");
-return 0;
-}
